93ed6fd21cd7b3cccdea46b646a475532fbb1b25
Frank Habermann <lordlamer@lordlamer.de>
2017-12-16 22:44:36 +0100

    * added liquibase config

--- src/main/java/org/knowledgeroot/app/config/LiquibaseConfig.java
+++ src/main/java/org/knowledgeroot/app/config/LiquibaseConfig.java
@@ -1,4 +1,21 @@
 package org.knowledgeroot.app.config;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class LiquibaseConfig {
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:dbupdates/changelog.xml");
+        liquibase.setContexts("development, production");
+        liquibase.setShouldRun(true);
+
+        return liquibase;
+    }
 }
