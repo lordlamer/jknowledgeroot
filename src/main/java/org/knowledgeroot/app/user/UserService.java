76f37878640df297ebd8f557470ec8f67acf6d14
Frank Habermann <lordlamer@lordlamer.de>
2017-12-24 00:09:56 +0100

    * create user restcontroller

--- src/main/java/org/knowledgeroot/app/user/UserService.java
+++ src/main/java/org/knowledgeroot/app/user/UserService.java
@@ -1,4 +1,36 @@
 package org.knowledgeroot.app.user;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    public List<User> findAllUsers() {
+        return null;
+    }
+
+    public User findById(long id) {
+        return null;
+    }
+
+    public boolean isUserExist(User user) {
+        return false;
+    }
+
+    public void saveUser(User user) {
+
+    }
+
+    public void updateUser(User user) {
+
+    }
+
+    public void deleteAllUsers() {
+
+    }
+
+    public void deleteUserById(long id) {
+
+    }
 }
--- src/main/java/org/knowledgeroot/app/user/controller/UserRestController.java
+++ src/main/java/org/knowledgeroot/app/user/controller/UserRestController.java
@@ -2,31 +2,120 @@ package org.knowledgeroot.app.user.controller;
 
 import org.knowledgeroot.app.config.OrikaMapper;
 import org.knowledgeroot.app.user.User;
+import org.knowledgeroot.app.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/user")
 public class UserRestController {
     @Autowired
     protected OrikaMapper mapper;
 
-    @RequestMapping("/test")
-    public UserDto test() {
-        User user = new User();
-        UserDto userDto = new UserDto();
+    @Autowired
+    protected UserService userService;
 
-        user.setEmail("me@la.de");
-        user.setId(1);
-        user.setActive(true);
-        user.setFirstName("foo");
-        user.setLastName("bar");
-        user.setLogin("foobar");
-        user.setLanguage("xxx");
+    //-------------------Retrieve All Users--------------------------------------------------------
 
-        userDto = mapper.map(user, UserDto.class);
+    @RequestMapping(value = "/user/", method = RequestMethod.GET)
+    public ResponseEntity<List<UserDto>> listAllUsers() {
+        List<User> users = userService.findAllUsers();
 
-        return userDto;
+        List<UserDto> userDtos = mapper.mapAsList(users, UserDto.class);
+
+        if(userDtos.isEmpty()){
+            return new ResponseEntity<List<UserDto>>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<List<UserDto>>(userDtos, HttpStatus.OK);
+    }
+
+
+    //-------------------Retrieve Single User--------------------------------------------------------
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDto> getUser(@PathVariable("id") long id) {
+        User user = userService.findById(id);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+
+        if (userDto == null) {
+            return new ResponseEntity<UserDto>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<UserDto>(userDto, HttpStatus.OK);
+    }
+
+
+    //-------------------Create a User--------------------------------------------------------
+
+    @RequestMapping(value = "/user/", method = RequestMethod.POST)
+    public ResponseEntity<Void> createUser(@RequestBody UserDto userDto, UriComponentsBuilder ucBuilder) {
+
+        User user = mapper.map(userDto, User.class);
+
+        if (userService.isUserExist(user)) {
+            return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+        }
+
+        userService.saveUser(user);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/user/{id}").buildAndExpand(user.getId()).toUri());
+
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+    }
+
+
+    //------------------- Update a User --------------------------------------------------------
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<UserDto> updateUser(@PathVariable("id") long id, @RequestBody UserDto userDto) {
+        if(id != userDto.getId()) {
+            return new ResponseEntity<UserDto>(HttpStatus.CONFLICT);
+        }
+
+        User currentUser = userService.findById(id);
+
+        if (currentUser==null) {
+            return new ResponseEntity<UserDto>(HttpStatus.NOT_FOUND);
+        }
+
+        currentUser = mapper.map(userDto, User.class);
+
+        userService.updateUser(currentUser);
+
+        return new ResponseEntity<UserDto>(userDto, HttpStatus.OK);
+    }
+
+    //------------------- Delete a User --------------------------------------------------------
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<UserDto> deleteUser(@PathVariable("id") long id) {
+        User user = userService.findById(id);
+
+        if (user == null) {
+            return new ResponseEntity<UserDto>(HttpStatus.NOT_FOUND);
+        }
+
+        userService.deleteUserById(id);
+
+        return new ResponseEntity<UserDto>(HttpStatus.NO_CONTENT);
+    }
+
+
+    //------------------- Delete All Users --------------------------------------------------------
+
+    @RequestMapping(value = "/user/", method = RequestMethod.DELETE)
+    public ResponseEntity<UserDto> deleteAllUsers() {
+        userService.deleteAllUsers();
+
+        return new ResponseEntity<UserDto>(HttpStatus.NO_CONTENT);
     }
 }
