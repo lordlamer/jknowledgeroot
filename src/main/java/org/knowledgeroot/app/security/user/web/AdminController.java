78c41843443c3d4f12dc70bc504382a11286f0d8
Frank Habermann <lordlamer@lordlamer.de>
2024-10-08 23:07:05 +0200

    * implemented user listing

--- src/main/java/org/knowledgeroot/app/security/user/web/AdminController.java
+++ src/main/java/org/knowledgeroot/app/security/user/web/AdminController.java
@@ -1,13 +1,28 @@
 package org.knowledgeroot.app.security.user.web;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.knowledgeroot.app.security.user.api.filter.UserFilter;
+import org.knowledgeroot.app.security.user.domain.UserDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
+@RequiredArgsConstructor
 public class AdminController {
+    private final UserDao userImpl;
+
     @RequestMapping("/admin/users")
     public String showUsers(Model model) {
+        model.addAttribute(
+                "users",
+                userImpl.listUsers(
+                        new UserFilter()
+                )
+        );
+
         return "admin/users";
     }
 
